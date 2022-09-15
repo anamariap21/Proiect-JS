09f6a34cf7ba2e07c4c68197c4292784bf82eee5
anamariap21 <anapacurar321@gmail.com>
2022-09-15 18:15:27 +0300

    Commit

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+});
+
 const form = document.getElementById('form');
 
 form.addEventListener('focus', (event) => {
--- src/email.js
+++ src/email.js
@@ -1,4 +1,11 @@
 
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+});
+
 const email= document.querySelector('input[type="email"]');
 
 email.addEventListener('focus', (event) => {
